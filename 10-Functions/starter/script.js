c671ebd8584b66e49e596baa1eccca4003d01753
Woobiweb <brunoitaliano.developer@gmail.com>
2021-02-05 10:54:48 +0100

    end of lesson 128

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassenger = 1, price = 199) {
+    //Default values in old way before ES6
+    // numPassenger = numPassenger || 1;
+    // price = price || 199;
+
+    //Default values in ES6 way directly in arguments and are DINAMIC
+    // so, for example: function (flightNum, numPassenger = 1, price = 199 * numPassenger)
+
+
+    const booking = {
+        flightNum,
+        numPassenger,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123', 2);
+createBooking('LH123', undefined,  2); //trucco per saltare parametro
