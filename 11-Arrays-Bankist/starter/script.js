c8252801728748c1782ce2f40f30a81885087efd
Woobiweb <brunoitaliano.developer@gmail.com>
2021-03-20 20:06:06 +0100

    end of 148

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -110,6 +110,8 @@ console.log(arr.slice(2));
 // slice -> oppure restituisce risultati in un range: L' indice zero-base indica dove finisce l'intervallo da selezionare.
 // slice seleziona gli elementi fino a quell'indice ma non l'elemento all'indice end.
 
+console.log(arr.slice(1,5); // 'b', 'c', 'd', 'e'
+
 console.log(arr.slice(2, 4)); // 'c', 'd'
 
 //per iniziare lo slice dal termine dell'array
@@ -189,7 +191,7 @@ movements.forEach(function(mov, i , arr){
 })
 
 //foreach NON é controllabile con break / continue. Se necessari utilizzare l'altro metodo
-*/
+
 
 //MAPS & SETS
 
@@ -211,6 +213,58 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, key, map) {
     console.log(`${key}: ${value}`);
 });
+*/
+
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const setJulia1 = [3, 5, 2, 12, 7];
+const setJulia2 = [9, 16, 6, 8, 3];
+const setKate1 = [4, 1, 15, 8, 3];
+const setKate2 = [10, 5, 6, 1, 4];
+
+let checkDogs = function (set1, set2) {
+    let juliaDogs = set1.slice(1, 5);
+    let juliaDogsDef = juliaDogs.slice(0, 2);
+    // con splice
+    /*
+    juliaDogs.splice();
+    juliaDogsDef.splice(0,1); // elimina il primo
+    juliaDogsDef.splice(-2); // elimina gli ultimi 2
+    */
+    let dogs = juliaDogsDef.concat(setKate1);
+
+
+    dogs.forEach(function (value, key, map) {
+            if (value > 2) {
+                console.log(`Dog number ${key + 1} is an adult.`)
+            } else {
+                console.log(`Dog number ${key + 1} is a puppy.`)
+            }
+        }
+    )
+}
+
+checkDogs(setJulia2, setKate2);
 
 
 
