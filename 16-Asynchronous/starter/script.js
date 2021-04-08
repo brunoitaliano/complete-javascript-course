4ee88d0d549f1f831aab9b7500bbe655f064c289
Woobiweb <brunoitaliano.developer@gmail.com>
2021-04-08 16:28:50 +0200

    end of lesson 257

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -166,7 +166,8 @@ Promise.resolve('Resolved promise 1').then(res => console.log(res)); // ordine d
 console.log('Test end'); // ordine di esecuzione: 2
 */
 
-//la function in Primise é la "execution function" e determina la asicronicità della funzione
+/*
+//la function in Promise é la "execution function" e determina la asicronicità della funzione
 const lotteryPromise = new Promise(function(resolve, reject) {
 
   console.log('Lottery draw is happening 🔮');
@@ -213,7 +214,47 @@ wait(2).then(() => {
 
 Promise.resolve('abc').then(x => console.log(x));
 Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+*/
 
+console.log('Getting position'); // happen first
+const getPosition = function() {
+  return new Promise(function(resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    // equivalente
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+// getPosition().then(pos => console.log(pos));
+
+const whereAmI = function() {
+  getPosition().then(pos => {
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+  })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+whereAmI();
 
 
 
